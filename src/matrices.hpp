68504acc9234fe35dc3884b7310bb7215fd95103
Ed Carney <carneyedwardj@gmail.com>
2024-04-06 18:54:33 -0400

    Adds matrci methods

--- src/matrices.hpp
+++ src/matrices.hpp
@@ -8,6 +8,10 @@ namespace matrices {
 	typedef std::vector<std::vector<int>> matrix_2d;
 
 	matrix_2d multiply_square_bruteforce(matrix_2d A, matrix_2d B);
+
+	matrix_2d multiply_square_dnc(matrix_2d A, matrix_2d B);
+
+	matrix_2d multiply_square_strassen(matrix_2d A, matrix_2d B);
 }
 
 #endif // MATRICES_HEADER_HPP
