b512c81642a62caf11d6f5adab300d2f54dec84b
Ed Carney <carneyedwardj@gmail.com>
2024-04-06 18:00:54 -0400

    Scaffolds initial matrix functionality

---
+++ src/matrices.cpp
@@ -0,0 +1,10 @@
+#include "matrices.hpp"
+#include <vector>
+
+namespace matrices {
+
+	matrix_2d multiply_square(matrix_2d A, matrix_2d B) {
+		std::vector<std::vector<int>> result
+		return result;
+	}
+}
---
+++ src/matrices.hpp
@@ -0,0 +1,13 @@
+#ifndef MATRICES_HEADER_HPP
+#define MATRICES_HEADER_HPP
+
+#include <vector>
+
+namespace matrices {
+
+	typedef std::vector<std::vector<int>> matrix_2d;
+
+	matrix_2d multiply_square_bruteforce(matrix_2d A, matrix_2d B);
+}
+
+#endif // MATRICES_HEADER_HPP
