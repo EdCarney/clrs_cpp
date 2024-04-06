566fe0e8334d454d9f0a8e4b8d2863ff815efe18
Ed Carney <carneyedwardj@gmail.com>
2024-04-05 23:49:57 -0400

    Adds divide-and-conquer approach for max subarray problem

--- src/arrays.cpp
+++ src/arrays.cpp
@@ -1,5 +1,9 @@
 #include "arrays.hpp"
 #include <limits>
+#include <cstdio>
+
+arrays::array_def max_subarray_dnc_internal(int[], int, int);
+arrays::array_def max_subarray_dnc_maxcrossing(int[], int, int, int);
 
 namespace arrays {
 	// Calculates the max subarry in the specified array.
@@ -27,7 +31,10 @@ namespace arrays {
 	// Uses an O(n*lgn) divide-and-conquer approach.
 	// Returns a tuple of (max, start_index, end_index)
 	array_def max_subarray_dnc(int arr[], int size) {
-		return std::make_tuple(0, 0, 0);
+		if (size == 0) {
+			return std::make_tuple(constants::INT_MIN, -1, -1);
+		}
+		return max_subarray_dnc_internal(arr, 0, size - 1);
 	}
 	
 	// Calculates the max subarry in the specified array.
@@ -36,4 +43,55 @@ namespace arrays {
 	array_def max_subarray_singlepass(int arr[], int size) {
 		return std::make_tuple(0, 0, 0);
 	}
+}
+
+arrays::array_def max_subarray_dnc_internal(int arr[], int low, int high) {
+	// check for bottom-out
+	if (low == high) {
+		return std::make_tuple(arr[low], low, high);
+	}
+
+	int mid = (low + high) / 2;
+
+	int max_left, low_left, high_left;
+	int max_right, low_right, high_right;
+	int max_cross, low_cross, high_cross;
+
+	std::tie(max_left, low_left, high_left) = max_subarray_dnc_internal(arr, low, mid);
+	std::tie(max_right, low_right, high_right) = max_subarray_dnc_internal(arr, mid + 1, high);
+	std::tie(max_cross, low_cross, high_cross) = max_subarray_dnc_maxcrossing(arr, low, mid, high);
+	
+	if (max_left > max_right && max_left > max_cross) {
+		return std::make_tuple(max_left, low_left, high_left);
+	} else if (max_right > max_left && max_right > max_cross) {
+		return std::make_tuple(max_right, low_right, high_right);
+	} else {
+		return std::make_tuple(max_cross, low_cross, high_cross);
+	}
+}
+
+arrays::array_def max_subarray_dnc_maxcrossing(int arr[], int low, int mid, int high) {
+	int sum, max_L, max_R;
+
+	int sum_L = arrays::constants::INT_MIN;
+	sum = 0;
+	for (int i = mid; i >= low; i--) {
+		sum += arr[i];
+		if (sum > sum_L) {
+			max_L = i;
+			sum_L = sum;
+		}
+	}
+
+	int sum_R = arrays::constants::INT_MIN;
+	sum = 0;
+	for (int i = mid + 1; i <= high; i++) {
+		sum += arr[i];
+		if (sum > sum_R) {
+			max_R = i;
+			sum_R = sum;
+		}
+	}
+	return std::make_tuple(sum_L + sum_R, max_L, max_R);
+
 }
--- test/arrays_tests.cpp
+++ test/arrays_tests.cpp
@@ -38,3 +38,39 @@ TEST(MaxSubarrayBruteForce, MultipleElementArray) {
 	EXPECT_EQ(start, 7);
 	EXPECT_EQ(end, 10);
 }
+
+TEST(MaxSubarrayDivideAndConquer, ZeroElementArray) {
+	int arr[] = {};
+	int size = 0;
+
+	int max, start, end;
+	std::tie(max, start, end) = arrays::max_subarray_dnc(arr, size);
+	
+	EXPECT_EQ(max, INT_MIN);
+	EXPECT_EQ(start, -1);
+	EXPECT_EQ(end, -1);
+}
+
+TEST(MaxSubarrayDivideAndConquer, SingleElementArray) {
+	int arr[] = { 1 };
+	int size = 1;
+
+	int max, start, end;
+	std::tie(max, start, end) = arrays::max_subarray_dnc(arr, size);
+	
+	EXPECT_EQ(max, arr[0]);
+	EXPECT_EQ(start, 0);
+	EXPECT_EQ(end, 0);
+}
+
+TEST(MaxSubarrayDivideAndConquer, MultipleElementArray) {
+	int arr[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+	int size = 16;
+
+	int max, start, end;
+	std::tie(max, start, end) = arrays::max_subarray_dnc(arr, size);
+	
+	EXPECT_EQ(max, 43);
+	EXPECT_EQ(start, 7);
+	EXPECT_EQ(end, 10);
+}
