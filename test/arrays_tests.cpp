9b9042d0fe8adcb50c86e46ec65c6e3ce07c437b
Ed Carney <carneyedwardj@gmail.com>
2024-04-05 23:13:32 -0400

    Adds non-trivial test case for max subarray

--- test/arrays_tests.cpp
+++ test/arrays_tests.cpp
@@ -3,8 +3,6 @@
 #include <tuple>
 #include <limits>
 
-//const int INT_MIN = std::numeric_limits<int>::min();
-
 TEST(MaxSubarrayBruteForce, ZeroElementArray) {
 	int arr[] = {};
 	int size = 0;
@@ -27,4 +25,16 @@ TEST(MaxSubarrayBruteForce, SingleElementArray) {
 	EXPECT_EQ(max, arr[0]);
 	EXPECT_EQ(start, 0);
 	EXPECT_EQ(end, 0);
+}
+
+TEST(MaxSubarrayBruteForce, MultipleElementArray) {
+	int arr[] = { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+	int size = 16;
+
+	int max, start, end;
+	std::tie(max, start, end) = arrays::max_subarray_bruteforce(arr, size);
+	
+	EXPECT_EQ(max, 43);
+	EXPECT_EQ(start, 7);
+	EXPECT_EQ(end, 10);
 }
