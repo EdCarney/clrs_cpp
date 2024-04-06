92d89b409236971aa178c4262ee8d0c5a0bad481
Ed Carney <carneyedwardj@gmail.com>
2024-04-05 23:13:41 -0400

    Formatting

--- src/sorting.cpp
+++ src/sorting.cpp
@@ -3,51 +3,54 @@
 void merge_sort_internal(int[], int, int);
 void merge_combine(int[], int, int, int);
 
-// Sorts the provided array using insertion sort
-void sorting::insertion_sort(int arr[], int size) {
-	for (int i = 1; i < size; i++) {
-		int v = arr[i];
-		int j = i - 1;
-		while (j >= 0 && arr[j] > v) {
-			arr[j + 1] = arr[j];
-			--j;
-		}
-		arr[j + 1] = v;
-	}
-}
+namespace sorting {
 
-// Sorts the provided array using selection sort
-void sorting::selection_sort(int arr[], int size) {
-	for (int i = 0; i < size - 1; i++) {
-		int min_value = arr[i];
-		int min_index = i;
-		for (int j = i + 1; j < size; j++) {
-			if (arr[j] < min_value) {
-				min_value = arr[j];
-				min_index = j;
+	// Sorts the provided array using insertion sort
+	void insertion_sort(int arr[], int size) {
+		for (int i = 1; i < size; i++) {
+			int v = arr[i];
+			int j = i - 1;
+			while (j >= 0 && arr[j] > v) {
+				arr[j + 1] = arr[j];
+				--j;
+			}
+			arr[j + 1] = v;
+		}
+	}
+	
+	// Sorts the provided array using selection sort
+	void selection_sort(int arr[], int size) {
+		for (int i = 0; i < size - 1; i++) {
+			int min_value = arr[i];
+			int min_index = i;
+			for (int j = i + 1; j < size; j++) {
+				if (arr[j] < min_value) {
+					min_value = arr[j];
+					min_index = j;
+				}
+			}
+			int temp = arr[i];
+			arr[i] = min_value;
+			arr[min_index] = temp;
+		}
+	}
+	
+	void bubble_sort(int arr[], int size) {
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = size - 1; j > i; j--) {
+				if (arr[j - 1] > arr[j]) {
+					int temp = arr[j];
+					arr[j] = arr[j - 1];
+					arr[j - 1] = temp;
+				}
 			}
 		}
-		int temp = arr[i];
-		arr[i] = min_value;
-		arr[min_index] = temp;
 	}
-}
-
-void sorting::bubble_sort(int arr[], int size) {
-	for (int i = 0; i < size - 1; i++) {
-		for (int j = size - 1; j > i; j--) {
-			if (arr[j - 1] > arr[j]) {
-				int temp = arr[j];
-				arr[j] = arr[j - 1];
-				arr[j - 1] = temp;
-			}
-		}
+	
+	// Sorts the provided array using merge sort
+	void merge_sort(int arr[], int size) {
+		merge_sort_internal(arr, 0, size - 1);
 	}
-}
-
-// Sorts the provided array using merge sort
-void sorting::merge_sort(int arr[], int size) {
-	merge_sort_internal(arr, 0, size - 1);
 }
 
 void merge_sort_internal(int arr[], int p, int q) {
