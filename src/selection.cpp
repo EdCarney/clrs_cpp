29e68a038b817023eccc70718a86563a8bdd6af6
Ed Carney <carneyedwardj@gmail.com>
2024-04-20 10:52:37 -0400

    Adds initial selection algos

---
+++ src/selection.cpp
@@ -0,0 +1,9 @@
+#include "selection.hpp"
+#include <vector>
+
+namespace selection {
+
+    int randomized_select(std::vector<int> vec, int i) {
+        return 0;
+    }
+}
---
+++ src/selection.hpp
@@ -0,0 +1,11 @@
+#ifndef SELECTION_HEADER_HPP
+#define SELECTION_HEADER_HPP
+
+#include <vector>
+
+namespace selection {
+
+    int randomized_select(std::vector<int> vec);
+}
+
+#endif // SELECTION_HEADER_HPP 
