da70272bba2980060365bd7cec04abd93b6902a0
Ed Carney <carneyedwardj@gmail.com>
2024-04-21 08:49:05 -0400

    Fixes some issues in the selection algo

--- src/selection.cpp
+++ src/selection.cpp
@@ -3,10 +3,12 @@
 #include <vector>
 
 int randomized_select_internal(std::vector<int> vec, int start, int end, int i);
-int randomized_partition(std::vector<int> vec, int start, int end);
+int randomized_partition(std::vector<int> &vec, int start, int end);
 
 namespace selection {
 
+    // Finds the i-th largest element in the provided vector
+    // The value of i should range between 0 and one less than the vector size.
     int randomized_select(std::vector<int> vec, int i) {
         return randomized_select_internal(vec, 0, vec.size() - 1, i);
     }
@@ -16,45 +18,37 @@ int randomized_select_internal(std::vector<int> vec, int start, int end, int i) 
     int len = end - start + 1;
 
     // check for bottom-out
-    if (len == 1) {
-        return vec[start];
+    if (len <= 1) {
+        return vec.at(start);
     }
 
     int q = randomized_partition(vec, start, end);
 
     if (i == q) {
-        return vec[q];
+        return vec.at(i);
     } else if (i < q) {
         return randomized_select_internal(vec, start, q - 1, i);
     }
     else {
-        return randomized_select_internal(vec, start, q - 1, i);
+        return randomized_select_internal(vec, q + 1, end, i - q - 1);
     }
 }
 
-// consider the input vector from start to end indexes
-// we are looking for the i-th element
-// for the partition step, we will:
-//// randomly select an index in our range
-//// swap that element with the end
-//// do our paritition step
-//// at the end, the pivot is in the right location
-//// we return the index of the pivot
-int randomized_partition(std::vector<int> vec, int start, int end) {
+int randomized_partition(std::vector<int> &vec, int start, int end) {
     int len = end - start + 1;
     int pivot_ind = start + std::rand() % len;
     
     // move pivot to front
-    std::swap(vec[pivot_ind], vec[end]);
-    int pivot = vec[end];
+    std::swap(vec.at(pivot_ind), vec.at(end));
+    int pivot = vec.at(end);
 
     // one-pass over vector
     int i = start - 1, j = start;
     while (++i < end) {
-        if (vec[i] < pivot) {
-            std::swap(vec[i], vec[j++]);
+        if (vec.at(i) < pivot) {
+            std::swap(vec.at(i), vec.at(j++));
         }
     }
-    std::swap(vec[j], vec[end]);
+    std::swap(vec.at(j), vec.at(end));
     return j;
 }
--- src/selection.hpp
+++ src/selection.hpp
@@ -5,7 +5,7 @@
 
 namespace selection {
 
-    int randomized_select(std::vector<int> vec);
+    int randomized_select(std::vector<int> vec, int i);
 }
 
 #endif // SELECTION_HEADER_HPP 
