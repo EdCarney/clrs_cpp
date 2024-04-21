8d3d403fb7ac79b5f17fe8a738a1479c27fa038a
Ed Carney <carneyedwardj@gmail.com>
2024-04-21 10:51:08 -0400

    Adds some more debug utilities

--- src/debug_utilities.cpp
+++ src/debug_utilities.cpp
@@ -1,20 +1,29 @@
+#include "debug_utilities.hpp"
 #include <vector>
 #include <iostream>
 
 namespace debug_utilities {
 
     void print_array(int arr[], int size, char* msg) {
+        print_array(arr, 0, size - 1, msg);
+    }
+            
+    void print_vector(std::vector<int> vec, std::string msg) {
+        print_vector(vec, 0, vec.size() - 1, msg);
+    }
+
+    void print_array(int arr[], int start, int end, char* msg) {
         printf("%s: [ ", msg);
-        for (int i = 0; i < size; i++) {
+        for (int i = start; i <= end; i++) {
             printf("%d, ", arr[i]);
         }
         printf("]\n");
     }
             
-    void print_vector(std::vector<int> vec, std::string msg) {
+    void print_vector(std::vector<int> vec, int start, int end, std::string msg) {
         std::cout << msg << ": [ ";
-        for (auto &elm : vec) {
-            std::cout << elm << ", ";
+        for (int i = start; i <= end; i++) {
+            std::cout << vec.at(i) << ", ";
         }
         std::cout << "]" << std::endl;
     }
--- src/debug_utilities.hpp
+++ src/debug_utilities.hpp
@@ -5,6 +5,10 @@ namespace debug_utilities {
 
     void print_array(int arr[], int size, char* msg);
 
+    void print_array(int arr[], int start, int end, char* msg);
+
     void print_vector(std::vector<int> vec, std::string msg);
+
+    void print_vector(std::vector<int> vec, int start, int end, std::string msg);
 
 }
