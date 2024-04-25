4c98190bae7d1e4d83a8dfae1c25954fe7eadf93
Ed Carney <carneyedwardj@gmail.com>
2024-04-24 23:20:35 -0400

    Adds initial implementation of simple heaps

---
+++ src/data_structures/simple_heaps.cpp
@@ -0,0 +1,100 @@
+#include "simple_heaps.hpp"
+#include <cmath>
+#include <string>
+#include <vector>
+#include <stdexcept>
+
+namespace heaps {
+
+    heap::heap(heapType type, int size) : _type(type), _heap_size(size) {
+        _heap_arr = std::vector<node*>(_heap_size);
+
+        for (auto &e : _heap_arr) {
+            e = nullptr;
+        }
+    }
+
+    heap::~heap() {
+        for (auto &e : _heap_arr) {
+            if (e != nullptr) {
+                node *curr_node = e, *prev_node;
+                while (curr_node != nullptr) {
+                    prev_node = curr_node;
+                    curr_node = curr_node->next;
+                    delete prev_node->data;
+                    delete prev_node;
+                }
+                delete[] e;
+            }
+        }
+    }
+
+    void heap::insert_data(entry *data) {
+        int hash_val = get_hash(data->key);
+        node *new_node = new node { nullptr, nullptr, data }; 
+
+        if (_heap_arr[hash_val] != nullptr) {
+            _heap_arr[hash_val]->prev = new_node;
+            new_node->next = _heap_arr[hash_val];
+        }
+
+        _heap_arr[hash_val] = new_node;
+    }
+
+    void heap::delete_data(entry *data) {
+        int hash_val = get_hash(data->key);
+        node *to_delete = search_nodes(data->key);
+
+        // noop if not found
+        if (to_delete == nullptr) {
+            return;
+        }
+
+        if (to_delete->next != nullptr) {
+            to_delete->next->prev = to_delete->prev;
+        }
+
+        if (to_delete->prev != nullptr) {
+            to_delete->prev->next = to_delete->next;
+        }
+
+        delete to_delete;
+    }
+
+    entry *heap::search_data(std::string key) {
+        node *curr_node = search_nodes(key);
+        return curr_node == nullptr ? nullptr : curr_node->data;
+    }
+
+    int heap::get_hash(std::string key) {
+        int key_int = 0;
+
+        for (int i = 0; i < key.length(); ++i) {
+            key_int += (wchar_t)key[i];
+        }
+
+        switch (_type) {
+            case heaps::Multiplication: {
+                double temp = MULT_FACTOR * (double)key_int;
+                return floor(_heap_size * (temp - floor(temp)));
+            }
+            case heaps::Division: {
+                return key_int % 1567;
+            }
+            default: {
+                throw std::runtime_error("Invalid heap type");
+            }
+        }
+    }
+
+    node *heap::search_nodes(std::string key) {
+        int hash_val = get_hash(key);
+
+        node *curr_node = _heap_arr[hash_val];
+        while (curr_node != nullptr && curr_node->data->key != key) {
+            curr_node = curr_node->next;
+        }
+        
+        return curr_node;
+    }
+}
---
+++ src/data_structures/simple_heaps.hpp
@@ -0,0 +1,46 @@
+#ifndef SIMPLE_HEAP_HPP_INCLUDED
+#define SIMPLE_HEAP_HPP_INCLUDED
+
+#include <string>
+#include <vector>
+
+namespace heaps {
+
+    enum heapType {
+        Multiplication,
+        Division,
+    };
+
+    struct entry {
+        std::string key;
+        int val;
+    };
+
+    struct node {
+        node *next;
+        node *prev;
+        entry *data;
+    };
+
+    class heap {
+        public:
+            heap(heapType type, int size);
+            ~heap();
+
+            void insert_data(entry *data);
+            void delete_data(entry *data);
+            entry *search_data(std::string key);
+
+        private:
+            const double MULT_FACTOR = 0.6180339887;
+
+            std::vector<node*> _heap_arr;
+            int _heap_size;
+            heapType _type;
+
+            int get_hash(std::string key);
+            node *search_nodes(std::string key);
+    };
+}
+
+#endif // SIMPLE_HEAP_HPP_INCLUDED
